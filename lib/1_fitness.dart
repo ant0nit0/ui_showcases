691a750a3c74b0a8ec973b92d57f78d96af05b29
ant0nit0 <antoineandreani1@gmail.com>
2025-05-12 22:48:38 +0200

    🏃‍♀️ 1. Fitness home screen

---
+++ lib/1_fitness.dart
@@ -0,0 +1,218 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FitnessAppHomeScreen extends StatelessWidget {
+  const FitnessAppHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Positioned.fill(
+          child: Image.asset(
+            'assets/images/fitness_girl.png',
+            fit: BoxFit.cover,
+            alignment: Alignment.centerLeft,
+          ),
+        ),
+        Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: ColoredBox(
+              color: Colors.black.withValues(alpha: .4),
+            )),
+        Positioned(
+          top: MediaQuery.of(context).padding.top + 10,
+          bottom: 24,
+          left: 24,
+          right: 24,
+          child: const HomeBody(),
+        ),
+      ]),
+    );
+  }
+}
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withValues(alpha: .8),
+            shape: BoxShape.circle,
+          ),
+          padding: const EdgeInsets.all(12),
+          child: const Icon(
+            Icons.arrow_back,
+          ),
+        ),
+        Text(
+          'Time stack',
+          style: GoogleFonts.poppins(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withValues(alpha: .8),
+            shape: BoxShape.circle,
+          ),
+          padding: const EdgeInsets.all(12),
+          child: const Icon(
+            Icons.edit_outlined,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const HomeAppBar(),
+        const SizedBox(height: 40),
+        Text(
+          'Morning\nWorkout',
+          style: GoogleFonts.montserrat(
+            fontSize: 48,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+            height: 1.1,
+            letterSpacing: -.4,
+          ),
+        ),
+        const SizedBox(height: 24),
+        Row(
+          children: [
+            _makeContainer(Icons.star_outline_rounded, '74'),
+            const SizedBox(width: 16),
+            _makeContainer(Icons.star_outline_rounded, '74'),
+          ],
+        ),
+        const SizedBox(height: 40),
+        Row(
+          spacing: 8,
+          children: [
+            _makeHashtag('#hydratation'),
+            _makeHashtag('#lifestyle'),
+            _makeHashtag('#healthy'),
+          ],
+        ),
+        const SizedBox(height: 24),
+        Row(
+          children: [
+            const Icon(Icons.alarm, color: Colors.white, size: 14),
+            const SizedBox(width: 8),
+            Text(
+              'Morning activity',
+              style: GoogleFonts.montserrat(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+        Row(
+          children: [
+            const Icon(Icons.calendar_month_outlined,
+                color: Colors.white, size: 14),
+            const SizedBox(width: 8),
+            Text(
+              '4 successful days',
+              style: GoogleFonts.montserrat(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+          ],
+        ),
+        const Spacer(),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.play_arrow_rounded, color: Colors.black),
+              const SizedBox(width: 8),
+              Text(
+                'Start',
+                style: GoogleFonts.montserrat(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 24),
+      ],
+    );
+  }
+
+  Widget _makeContainer(IconData icon, String text) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        border: Border.all(color: Colors.white),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 12,
+          ),
+          const SizedBox(width: 8),
+          Text(
+            text,
+            style: GoogleFonts.montserrat(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _makeHashtag(String text) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withValues(alpha: .3),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      child: Text(
+        text,
+        style: GoogleFonts.montserrat(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
